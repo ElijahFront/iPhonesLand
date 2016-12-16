176e07de3f869c9f4b6a4ef304fabb07b51c26db
Ilya <cuprum19930@gmail.com>
2016-12-16 20:07:14 +0500

    added mailing, mouse parallax

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
   $('.form .button').on('click', function(e){
     e.preventDefault();
 
-    var name = $('input[name="upper_form_name"]').value(),
-        phone = $('input[name="upper_form_tel"]').value();
+    var $this = $(this);
 
-    $.post('send_email.php', {name:name, tel:phone})
+    var name = $('input[name="upper_form_name"]'),
+        phone = $('input[name="upper_form_tel"]');
+
+    if (name.val() != '' && phone.val() != ''){
+      $.post('send_email.php', $('.form').serialize(), function(data) {
+        console.log('ok');
+        name.val('');
+        phone.val('');
+        $this.val('Заявка отправлена!')
+      })
+    }
+
   })
 })();
 
@@ -37,4 +47,26 @@
       var expireDate = `${year}-${month}-${day} 00:00:00`;
 
       $('.promo_timer').attr('data-date', expireDate)
+})();
+
+
+// MOUSE PARALLAX
+
+
+(function() {
+  console.log('hey');
+  $(window).on('mousemove', function(e){
+    var mouseX = e.pageX,
+        mouseY = e.pageY,
+        w = (window.innerWidth/2),  // для изменения начальной точки центра на середину окна
+        h = (window.innerHeight/2),
+        MoveX = ((w*0.01) - mouseX)*0.05,
+        MoveY = ((h*0.01) - mouseY)*0.05;
+          console.log(MoveY);
+
+    $('.carralax-container').css({
+    'background-position' : `${MoveX}px center`});  // the main idea is here. it would also be conscious to set varieties out of the 'transform3d' property
+
+    });
+
 })();
